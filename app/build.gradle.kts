3526a8f9b78bde2fa55011e52a3af5b891fdfe74
VkctorC <VkctorC@users.noreply.github.com>
2026-09-25 14:35:04 -0300

    release: promote beta 2 fixes to rc.26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.pkgpocket.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 54
-        versionName = "1.0.0-beta.2"
+        versionCode = 55
+        versionName = "1.0.0-rc.26"
         buildConfigField("int", "BETA_MAX_GAMES", "5")
         buildConfigField("int", "BETA_MAX_DLCS", "5")
         buildConfigField("int", "BETA_MAX_UPDATES", "5")
